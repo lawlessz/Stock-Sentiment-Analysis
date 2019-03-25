6c8a9a1f5fe14a30ea415be9d21ee9a601addccd
Yikang Luo <luoy2@hotmail.com>
2019-03-25 17:22:46 -0400

    finish tweets to local

--- twitterListener/listener.py
+++ twitterListener/listener.py
@@ -2,43 +2,60 @@ import datetime
 import tweepy
 import logging
 import pandas as pd
+import json
 from configparser import RawConfigParser
 
-#TODO:
-# 1. continuously store json file into local
+
+# TODO:
+# 1. continuously store json file into local -- Done
 # 2. make a data writer thread to read stored json file to database
 # 3. automatic re-connect twitter listener
 
 import threading
+
 lock = threading.Lock()
+
 
 # 1. twitter break ->
 # 2. sql server break -> queue => pickle to pick up
 
 
-#https://www.dataquest.io/blog/streaming-data-python/
+# https://www.dataquest.io/blog/streaming-data-python/
 class StreamListener(tweepy.StreamListener):
     def __init__(self):
-        super(StreamListener,self).__init__()
-        self.file_name = datetime.datetime.now().strftime("%Y-%m-%d") + '.json'
-        self.file_handler = open('tweetsCache/' + self.file_name, 'w+')
+        super(StreamListener, self).__init__()
+        self.tweets_list = []
+        # self.file_handler = open('tweetsCache/tweets', 'w+')
+        # self.conn = sqlite3.connect('tweetsCache/tweets.db')
+        # check if table exists
+        # self.cursor = self.conn.cursor()
+        # check_query = "select name from sqlite_master where type = 'table';"
+        # self.cursor.execute(check_query)
+        # tables = self.cursor.fetchall()
+        # if "tweets" not in tables:
+        #     self.cursor.execute('''
+        #     CREATE TABLE tweets
+        #              (description text, loc text, text text, coordinates real, price real)
+        #              ''')
 
-
-    def on_status(self, status):
-        result_dict = {
-            'description': status.user.description,
-            'loc': status.user.location,
-            'text': status.text,
-            'coords': status.coordinates,
-            'name': status.user.screen_name,
-            'user_created_at': status.user.created_at,
-            'followers': status.user.followers_count,
-            'id_str': status.id_str,
-            'created_time': status.created_at,
-            'retweets': status.retweet_count,
-            'bg_color': status.user.profile_background_color,
-        }
-        print(result_dict)
+    def on_data(self, data):
+        # print(type(data))
+        try:
+            if len(self.tweets_list) < 1000:
+                self.tweets_list.append(json.loads(data))
+                pass
+            else:
+                print("saving cache....")
+                now = datetime.datetime.utcnow()
+                filename = now.strftime("tweetsCache/%Y%m%dT%H%M%S.json")
+                with open(filename, 'w+') as f:
+                    json.dump(self.tweets_list, f)
+                self.tweets_list = []
+            print(data)
+            return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
 
 
     def on_error(self, status_code):
@@ -54,8 +71,6 @@ def get_stock_ticks():
     return [i for i in stock_data['Symbol']]
 
 
-
-
 def main():
     config = RawConfigParser()
     config.read('credentials/config.ini')
@@ -64,14 +79,22 @@ def main():
     ACCESS_TOEKN_KEY = config['twitter']['access_token_key']
     ACCESS_TOKEN_SECRET = config['twitter']['access_token_secret']
 
-
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_TOEKN_KEY, ACCESS_TOKEN_SECRET)
     api = tweepy.API(auth)
     stream_listener = StreamListener()
     stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
     logging.info("starting twitter listener...")
-    stream.filter(track=['$' + i for i in get_stock_ticks()], languages=['en'])
+    stream.filter(track=['$' + i for i in get_stock_ticks()], languages=['en'], is_async=True)
+
+
+
+'''
+read data:
+file_name = 'tweetsCache/20190325T211635.json'
+with open(file_name, 'r') as f:
+    w=json.load(f)
+'''
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO,
