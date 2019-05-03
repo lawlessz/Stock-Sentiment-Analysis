10278e018dc0775dc549612c72af19513c76efbc
Yikang Luo <luoy2@hotmail.com>
2019-05-03 09:28:24 +0800

    using mysql pool instead of mysql connection to prevent connection drop

--- webApp/webApp/Server.js
+++ webApp/webApp/Server.js
@@ -7,139 +7,113 @@ var fs = require('fs');
 // var config = ini_.parse(fs.readFileSync('./credentials/mysql_password_db.cnf', 'utf-8'));
 
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/views/index.html');
-});
-
-//enter database info
-// var con = mysql.createConnection({
-//  host: config.ds410.host,
-//  user: config.ds410.user,
-//  password: config.ds410.password,
-//  database: "stock_prices"
-// });
-//
-// var conTweets = mysql.createConnection({
-//  host: config.ds410.host,
-//  user: config.ds410.user,
-//  password: config.ds410.password,
-//  database: "tweets_features"
-// });
-
-var poolStock  = mysql.createPool({
- host: "usdb.yikangquant.club",
- user: "cs498cca_project",
- password: "@GHhKMMP1EH4",
- database: "stock_prices"
-});
-
-var conStock = mysql.createConnection({
- host: "usdb.yikangquant.club",
- user: "cs498cca_project",
- password: "@GHhKMMP1EH4",
- database: "stock_prices"
-});
-
-var poolTweets  = mysql.createPool({
- host: "usdb.yikangquant.club",
- user: "cs498cca_project",
- password: "@GHhKMMP1EH4",
- database: "tweets_features"
-});
-
-var conTweets = mysql.createConnection({
- host: "usdb.yikangquant.club",
- user: "cs498cca_project",
- password: "@GHhKMMP1EH4",
- database: "tweets_features"
-});
-
-conStock.connect(function(err) {
-  if (err) throw err;
-  // console.log("Connected!");
-
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 
-conTweets.connect(function(err) {
-  if (err) throw err;
-  // console.log("Connected!");
-
+var pool = mysql.createPool({
+    host: "usdb.yikangquant.club",
+    user: "cs498cca_web",
+    password: "@cs498cca_web",
+    database: "stock_prices"
 });
 
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  /*conStock.query("SELECT * FROM stock_prices.AAPL WHERE OPEN != 'null'", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-  });*/
+io.on('connection', function (socket) {
+    console.log('a user connected');
 
-  socket.on('update', function(ticker) {
-  	var sql_statement = "SELECT * FROM stock_prices." + ticker.toUpperCase() + " WHERE OPEN != 'null'";
-  	conStock.query(sql_statement, function (err, result, fields) {
-	    if (err) throw err;
-		//console.log(result);
+    socket.on('update', function (ticker) {
+        var sql_statement = "SELECT * FROM stock_prices." + ticker.toUpperCase() + " WHERE OPEN != 'null'";
+        pool.getConnection(function (err, connection) {
+            // Use the connection
+            connection.query(sql_statement, function (err, result) {
+                if (err) {
+                    console.log(err);
+                }
+                ;
+                // And done with the connection.
+                var result2 = JSON.parse(JSON.stringify(result));
+                // console.log(result2);
+                var data = []
+                for (var i = 0; i < result2.length; i++) {
+                    data.push({'time': result2[i]['TIME'], 'value': result2[i]['CLOSE']});
+                }
+                socket.emit("priceData", {'data': data, 'ticker': ticker});
+                connection.release();
+                // Don't use the connection here, it has been returned to the pool.
+            });
+        });
 
-		var result2 = JSON.parse(JSON.stringify(result));
-		// console.log(result2);
-		var data = []
-		for (var i = 0; i < result2.length; i++)  {
-			data.push({'time': result2[i]['TIME'], 'value': result2[i]['CLOSE']});
-		}
-		socket.emit("priceData", {'data': data, 'ticker': ticker});
+    });
 
-	});
-  });
+    socket.on('updateTweetVolume', function (ticker) {
+        var sql_statement = "SELECT * FROM " + ticker.toUpperCase() + "_1H";
+        pool.getConnection(function (err, connection) {
+            // Use the connection
+            connection.query(sql_statement, function (err, result) {
+                if (err) {
+                    console.log(err);
+                }
+                // And done with the connection.
+                var result2 = JSON.parse(JSON.stringify(result));
+                var bear_dict = {}; //positive
+                var bull_dict = {}; //negative
+                var nuetral_dict = {}; //neutral
+                var temp;
+                var date;
+                for (var i = 0; i < result2.length; i++) {
+                    temp = result2[i];
+                    date = result2[i].DATETIME;
+                    bear_dict[date] = temp.BEARISH_COUNT;
+                    bull_dict[date] = temp.BULLISH_COUNT;
+                    nuetral_dict[date] = temp.NEUTRAL_COUNT;
+                }
+                socket.emit("tweetVolumeUpdate", {
+                    'ticker': ticker,
+                    'bear_dict': bear_dict,
+                    'bull_dict': bull_dict,
+                    'nuetral_dict': nuetral_dict
+                });
+                // Don't use the connection here, it has been returned to the pool.
+            });
+        });
 
-  socket.on('updateTweetVolume', function(ticker) {
-  	conTweets.query("SELECT * FROM " + ticker.toUpperCase() + "_1H", function (err, result, fields) {
-  		if (err) throw err;
-		var result2 = JSON.parse(JSON.stringify(result));
-  		var bear_dict = {}; //positive
-		var bull_dict = {}; //negative
-		var nuetral_dict = {}; //neutral
-		var temp;
-		var date;
-		for (var i = 0; i < result2.length; i++) {
-			temp = result2[i];
-			date = result2[i].DATETIME;
-			bear_dict[date] = temp.BEARISH_COUNT;
-			bull_dict[date] = temp.BULLISH_COUNT;
-			nuetral_dict[date] = temp.NEUTRAL_COUNT;
-		}
-		socket.emit("tweetVolumeUpdate", {'ticker': ticker, 'bear_dict': bear_dict, 'bull_dict': bull_dict, 'nuetral_dict':nuetral_dict});
-  	});
-  });
+    });
 
-  socket.on('regressionUpdate', function(ticker) {
-  	conTweets.query("SELECT * FROM " + ticker.toUpperCase() + "_1H_REGRESSION", function (err, result, fields) {
-  		if (err) throw err;
-  		var result2 = JSON.parse(JSON.stringify(result));
-  		var actual = {};
-  		var fitted = {};
-  		var temp;
-  		var time;
-  		for (var i = 0; i < result2.length; i++) {
-  			temp = result2[i];
-  			time = temp.time;
-  			actual[time] = temp.actual;
-  			fitted[time] = temp.fitted;
-  		}
-  		socket.emit('regressionUpdate', {'ticker': ticker, 'actual': actual, 'fitted': fitted});
-  	});
-  });
+    socket.on('regressionUpdate', function (ticker) {
+        sql_statement = "SELECT * FROM " + ticker.toUpperCase() + "_1H_REGRESSION";
+        pool.getConnection(function (err, connection) {
+            // Use the connection
+            connection.query(sql_statement, function (err, result) {
+                if (err) {
+                    console.log(err);
+                }
+                var result2 = JSON.parse(JSON.stringify(result));
+                var actual = {};
+                var fitted = {};
+                var temp;
+                var time;
+                for (var i = 0; i < result2.length; i++) {
+                    temp = result2[i];
+                    time = temp.time;
+                    actual[time] = temp.actual;
+                    fitted[time] = temp.fitted;
+                }
+                socket.emit('regressionUpdate', {'ticker': ticker, 'actual': actual, 'fitted': fitted});
+            });
+        });
+    });
 
-  socket.on('error', function(e){
-		console.log(e);
-  });
+    socket.on('error', function (e) {
+        console.log(e);
+    });
 
 
+})
+;
 
-});
 
-
-
-http.listen(30000, '0.0.0.0', function(){
-  console.log('listening on *:3000');
+http.listen(30000, '0.0.0.0', function () {
+    console.log('listening on *:3000');
 });
